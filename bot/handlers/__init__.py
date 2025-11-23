2e92c7f04d780a2330a490c2364860308fdb5938
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-11-23 03:27:28 +0300

    delete all

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,1 @@
 """Обработчики событий."""
-
-from . import navigation, task_input, task_confirmation, task_management, integrations_ui
-
-__all__ = [
-    "navigation",
-    "task_input",
-    "task_confirmation",
-    "task_management",
-    "integrations_ui"
-]
--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,10 +1,1 @@
 """Генерация всех inline-клавиатур."""
-
-from . import menu, task_input, destinations, task_actions
-
-__all__ = [
-    "menu",
-    "task_input",
-    "destinations",
-    "task_actions"
-]
