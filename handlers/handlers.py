f24f49be0e9cee0a5c6b05c8f0b093b0ba781e39
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-26 00:05:00 +0300

    add main inline keyboard

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,10 +1,17 @@
 from telebot import types
-
+import sqlite3
 
 def load_handlers(bot):
+    main_keyboard = types.InlineKeyboardMarkup() # создаем главную инлайн клаву в боте
+    button1 = types.InlineKeyboardButton("Все задачи", callback_data="all_lasks")
+    button2 = types.InlineKeyboardButton("Новая задача", callback_data="new_task")
+    main_keyboard.add(button1, button2)
+
     @bot.message_handler(commands=['start'])
     def start(message):
-        bot.reply_to(message, "Привет! Я бот лол кек\nЕсли хочешь добавить задачу, то вызови /new\n" \
-        "Если хочешь изменить задачу, то вызови /update\n" \
-        "Удалить - /delete\n" \
-        "Просмотреть список задач - /tasks")
+        bot.send_message(message.chat.id, "Пошел нахуй броук на бедном",
+        reply_markup=main_keyboard)
+
+    @bot.message_handler()
+    def add_new_task():
+        return
