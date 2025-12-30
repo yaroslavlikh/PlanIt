7e4e63b7264199b1c5b804613eb4665a07666829
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 21:43:05 +0300

    change llm version (from gemini 3 flash to latest vesion literally)

--- llm/llm.py
+++ llm/llm.py
@@ -3,7 +3,7 @@ from llm.prompt import prompt_editing
 from llm.gemini import client
 def ask_qwen(prompt):
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=prompt_editing + prompt
+        model="gemini-flash-latest", contents=prompt_editing + prompt
     )
     print(f'результат: {response.text}')
     return response.text
