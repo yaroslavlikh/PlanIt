1ef86bcdddcc5d842ddf2c711b53f5fa3c58eefd
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 21:38:28 +0300

    try/except for every main action

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import telebot
 from config import get_token_bot
 from handlers.handlers import load_handlers
-from db import init_db
+from tmp.db import init_db
 
 
 def start_app(token):
-    init_db()
-    bot = telebot.TeleBot(token=token)
-    load_handlers(bot)
-    print("Бот запустился...")
-    bot.polling()
+    try:
+        init_db()
+        print("База данных инициализирована")
+    except Exception as e:
+        print(f"Ошибка инициализации базы данных: {e}")
+        return
+    try:
+        bot = telebot.TeleBot(token=token)
+    except Exception as e:
+       print(f"Ошибка инициализации бота: {e}")
+       return
+    try:
+        load_handlers(bot)
+    except Exception as e:
+        print(f"Ошибка загрузки обработчиков: {e}")
+        return
+    try:
+        print("Бот работает...")
+        bot.infinity_polling()
+    except Exception as e:
+        print(f"Ошибка запуска бота: {e}")
 
 
 if __name__ == '__main__':
-    token = get_token_bot()
+    try:
+        token = get_token_bot()
+    except Exception as e:
+       print(f"Ошибка получения токена бота: {e}")
     start_app(token)
