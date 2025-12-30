988e1a23f8bfff70d9467cfee2f3749e7e13fdc1
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 21:39:39 +0300

    put database into tmp folder

---
+++ tmp/db.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('tmp/tasks.sql')
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS tasks(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL,
+        title TEXT NOT NULL,
+        start_date TEXT,
+        start_time TEXT,
+        end_time TEXT,
+        description TEXT,
+        type TEXT NOT NULL);""")
+    conn.commit()
+    conn.close()
