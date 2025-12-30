a03aca7093d0e158e53a5925f5679c72764b1311
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2025-12-30 21:43:57 +0300

    fix return info about tasks

--- handlers/return_task.py
+++ handlers/return_task.py
@@ -1,24 +1,37 @@
-def ret_cal(el): # возвращение календарной задачи
-    # el[0] - id, el[1] - user_id, el[2] - title, el[3] - start_time, el[4] - end_time, el[5] - description
+def ret_cal(el):
     task_parts = []
-    task_parts.append(f'Название: {el[2]}')
-    if el[3] and str(el[3]).strip():  # start_time
-        task_parts.append(f'Начало: {el[3]} в {el[4]}')
-    if el[4] and str(el[4]).strip():  # end_time
-        task_parts.append(f'Конец: {el[4]}')
-    if el[5] and str(el[5]).strip():  # description
-        task_parts.append(f'Описание: {el[5]}')
+
+    task_parts.append(f'Название: {el[0]}')
+
+    if el[1] and el[1].strip():
+        if el[2] and el[2].strip():
+            task_parts.append(f'Начало: {el[1]} в {el[2]}')
+        else:
+            task_parts.append(f'Начало: {el[1]}')
+
+    if el[3] and el[3].strip():
+        task_parts.append(f'Конец: {el[3]}')
+
+    if el[4] and el[4].strip():
+        task_parts.append(f'Описание: {el[4]}')
+
     print(task_parts, ', '.join(task_parts))
     return task_parts
 
-def ret_task(el): # возвращение задачи
+
+def ret_task(el):
     task_parts = []
-    task_parts.append(f'Название: {el[2]}')
-    if el[5] and str(el[5]).strip():  # description
-        task_parts.append(f'Описание: {el[5]}')
-    if el[3] and str(el[3]).strip() and str(el[3]).strip():  # start_time
-        task_parts.append(f'Когда надо сделать: {el[3]} в {el[4]}')
-    elif el[3] and str(el[3]).strip():
-        task_parts.append(f'Когда надо сделать: {el[3]}')
+
+    task_parts.append(f'Название: {el[0]}')
+
+    if el[4] and el[4].strip():
+        task_parts.append(f'Описание: {el[4]}')
+
+    if el[1] and el[1].strip():
+        if el[2] and el[2].strip():
+            task_parts.append(f'Когда надо сделать: {el[1]} в {el[2]}')
+        else:
+            task_parts.append(f'Когда надо сделать: {el[1]}')
+
     print(task_parts, ', '.join(task_parts))
     return task_parts
