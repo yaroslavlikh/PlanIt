395f4fa249da7fe888fcd4ebb40e68c15375d862
Yaroslav Likhachev <yaroslav_li07@mail.ru>
2026-01-14 22:52:33 +0300

    change return from llm to json

--- llm/llm.py
+++ llm/llm.py
@@ -3,7 +3,11 @@ from llm.prompt import prompt_editing
 from llm.gemini import client
 def ask_qwen(prompt):
     response = client.models.generate_content(
-        model="gemini-flash-latest", contents=prompt_editing + prompt
+        model="gemini-flash-latest", 
+        contents=prompt_editing + prompt,
+        generation_config={
+            "response_mime_type": "application/json"
+        }
     )
     print(f'результат: {response.text}')
     return response.text
