159b57499131e4eebc8ac2c21bca8c59e1ff212a
Yaroslav Likhachev <yaroslavlikh@Yaroslavs-MacBook-Air.local>
2026-01-16 16:05:49 +0300

    del : from verify task

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -55,12 +55,11 @@ def load_handlers(bot):
 
         if type_of == "расписание":
             ans = ret_cal(parsed_info)
-            ans = ', '.join(ans)
         else:
             ans = ret_task(parsed_info)
-            ans = ', '.join(ans)
+        ans = ', '.join(ans)
 
-        bot.send_message(message.chat.id, f'Добавляем в {type_of}?:\n{ans}', reply_markup=agree_task_keyboard)
+        bot.send_message(message.chat.id, f'Добавляем в {type_of}?\n{ans}', reply_markup=agree_task_keyboard)
 
     @bot.callback_query_handler(func=lambda call: call.data == "okey") # обработчик согласия с задачей
     def agree_task(call):
